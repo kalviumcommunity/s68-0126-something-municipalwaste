2c027df432946031f9bf20504a5743162bd82126
Mayur K Setty <settymayurk@gmail.com>
2026-02-03 20:03:55 +0530

    removed dashboard error

--- auth.config.ts
+++ auth.config.ts
@@ -4,5 +4,16 @@ export const authConfig = {
   pages: {
     signIn: "/login",
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+
+      if (isOnDashboard && !isLoggedIn) {
+        return false; // Redirect to login page
+      }
+      return true;
+    },
+  },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
